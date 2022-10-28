14f5f260cee718b67833a725018b00a9d13582e4
Kanan Amirov <amirovknn@gmail.com>
2022-10-28 22:11:06 +0400

    control structure - if and switch

---
+++ 10.28-If-Else-Switch-Case/AP207.js
@@ -0,0 +1,62 @@
+let age = 16;
+let city1 = "Gence"
+
+// if(age>=16){
+//     console.log("Xoş gəlmisiniz!!")
+// }else {
+//     console.log("Boyuyende gelersen :)")
+// }
+
+
+// if (age >= 16 )
+// {
+//     if(city1 == "Baku")
+//     {
+//         console.log("age is okay for subscription, city is Baku");
+//     }
+//     else
+//     {
+//         console.log("Yas uygundur amma seher uygun deyil");
+//     }
+// }
+// else {
+//     console.log("age is below 16");
+// }
+
+let weekDay = 26;
+
+// switch(weekDay){
+//     case 0: console.log("Today is Sunday");
+//     break;
+//     case 1: console.log("Today is Monday");
+//     break;
+//     case 2: console.log("Today is Tuesday");
+//     break;
+//     case 3: console.log("Today is Wednesday");
+//     break;
+//     case 4: console.log("Today is Thursday");
+//     break;
+//     case 5: console.log("Today is Friday");
+//     break;
+//     case 6: console.log("Today is Saturday");
+//     break;
+//     default: console.log("There is no such weekday")
+// }
+
+
+switch(weekDay){
+    case 1: console.log("Today is Monday");
+    break;
+    case 2: console.log("Today is Tuesday");
+    break;
+    case 3: console.log("Today is Wednesday");
+    break;
+    case 4: console.log("Today is Thursday");
+    break;
+    case 5: console.log("Today is Friday");
+    break;
+    case 6:
+    case 0: console.log("This is weekend");
+    break;
+    default: console.log("There is no such weekday")
+}
---
+++ 10.28-If-Else-Switch-Case/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+
+int age = 17;
+string city1 = "Gence";
+string city2 = "Sumqayit";
+
+//if (age >= 16 && city1 == "Gence")
+//{
+//    Console.WriteLine("age > 16, city is Baku");
+//}
+//else if (age >= 16 && city2 == "Sumqayit")
+//{
+//    Console.WriteLine("age > 16, city is Sum");
+//}
+//else
+//{
+//    Console.WriteLine("Test");
+//}
+
+//if (age >= 16)
+//{
+//    if (city1 == "Baku")
+//    {
+//        Console.WriteLine("age > 16, city is Baku");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Yas uygundur amma seher uygun deyi");
+//    }
+//}
+
+
+
+//if (age >= 16)
+//{
+//    Console.WriteLine("Xoş gəlmisiniz!!");
+//}
+//else
+//{
+//    Console.WriteLine("Boyuyende gelersen :)");
+//}
+
+
+int  weekDay = 5;
+
+switch(weekDay){
+    case 0: Console.WriteLine ("Today is Sunday");
+    break;
+    case 1:
+        Console.WriteLine("Today is Monday");
+    break;
+    case 2:
+        Console.WriteLine("Today is Tuesday");
+    break;
+    case 3:
+        Console.WriteLine("Today is Wednesday");
+    break;
+    case 4:
+        Console.WriteLine("Today is Thursday");
+    break;
+    case 5:
+        Console.WriteLine("Today is Friday");
+    break;
+    case 6:
+        Console.WriteLine("Today is Saturday");
+    break;
+    default:
+        Console.WriteLine("There is no such weekday");
+        break;
+}
