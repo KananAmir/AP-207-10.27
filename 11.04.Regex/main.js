ab39876a7188534ba48f8b8497bc2986fa198ee3
Kanan Amirov <amirovknn@gmail.com>
2022-11-04 23:22:07 +0400

    regex samples

---
+++ 11.04.Regex/main.js
@@ -0,0 +1,22 @@
+// compare 2 character within their ascii value
+// console.log("R" > "]");
+
+
+//testing regex pattern
+
+// let text = "abcccccccccaafaaaaaaa";
+// let pattern = /^[abc]+$/;
+// let result = pattern.test(text);
+
+// console.log(result);
+
+
+// replacing "Phyton" words with "Javascript"
+
+let txt = "Python is the most beautiful language that a human begin has ever created.\
+I recommend python for a first programming language";
+
+console.log(txt.replace(/python/gi, "Javascript"));
+
+
+
